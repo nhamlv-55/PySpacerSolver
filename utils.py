071ebf72e2441210f19a4786ce99f46cf034a829
Nham Van Le <nv3le@precious3.eng.uwaterloo.ca>
2020-03-04 11:07:14 -0500

    visualize to html page as well

--- utils.py
+++ utils.py
@@ -1,12 +1,19 @@
 from itertools import chain, combinations
 from termcolor import colored
 
-def visualize(cube, inducted_cube):
+def html_colored(text, color):
+    return '<p><font size="2" color="%s">%s</font></p>'%(text, color)
+
+def visualize(cube, inducted_cube, html_file = None):
     for l in cube:
         if l in inducted_cube:
-            print(colored("||\t"+str(l), 'green'))
+            print(colored("||\t"+str(l), 'blue'))
+            if html_file is not None:
+                html_file.write(html_colored(str(l), 'blue')
         else:
             print(colored("||\t"+str(l), 'red'))
+            if html_file is not None:
+                html_file.write(html_colored(str(l), 'red')
 
 def powerset(policy):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
